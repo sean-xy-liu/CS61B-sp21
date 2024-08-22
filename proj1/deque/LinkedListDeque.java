cab3c3fd2afc1802597eff67f2965bff6f68db3e
Sean Liu <liuxinyong083@gmail.com>
2024-08-22 21:57:29 +0800

    Finished the implementation of LListDeque

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,218 @@
+package deque;
+
+public class LinkedListDeque<T> {
+  private class Node{
+    T item;
+    Node next;
+    Node prev;
+    Node(T item, Node next){
+      this.item = item;
+      doubleLink(next);
+    }
+    Node(){
+      this.item = (T) new Object();
+      this.next = null;
+      this.prev = null;
+    }
+    Node(T item){
+      this.item = item;
+      this.next = null;
+      this.prev = null;
+    }
+
+    /**
+     * Set current node's next node to node other,
+     * and node other's prev node to current node.
+     * @param other
+     */
+    void doubleLink(Node other){
+        next = other;
+        if (other != null)
+          other.prev = this;
+    }
+    void reverseDoubleLink(Node other){
+      prev = other;
+      if (other != null)
+        other.next = this;
+    }
+
+    /**
+     * Bridge 2 nodes, front node's next point to current node, current node's next point to back node.
+     * So is the the prev pointer.
+     * If front and back are the same, forms a circular list with the node.
+     * @param front
+     * @param back
+     */
+    void bridgeNodes(Node front, Node back){
+      doubleLink(back);
+      reverseDoubleLink(front);
+    }
+  }
+  private Node first;
+  private Node last;
+  private Node sentinel = new Node();
+  private int size;
+  LinkedListDeque(T item){
+    addToEmpty(item);
+  }
+  LinkedListDeque(){
+    size = 0;
+  }
+  /**
+   * Helper function for addFirst and addLast.
+   * If add an item to an empty list, both first and last should be updated.
+   * @param item
+   */
+  private void addToEmpty(T item){
+    Node p = new Node(item);
+    p.bridgeNodes(sentinel, sentinel);
+    updateFirst();
+    updateLast();
+    size = 1;
+  }
+
+  private void updateFirst(){
+    first = sentinel.next;
+  }
+  private void updateLast(){
+    last = sentinel.prev;
+  }
+
+  public static void main(String[] args) {
+    LinkedListDeque<String> lls = new LinkedListDeque<>();
+    lls.addFirst("hello");
+    lls.addFirst("hi");
+    lls.addLast("halo");
+    lls.printDeque();
+  }
+
+  /**
+   * Adds an item of type T to the front of the deque.
+   * @param item
+   */
+  public void addFirst(T item){
+    if (isEmpty())
+      addToEmpty(item);
+    else {
+      Node p = new Node(item);
+      p.bridgeNodes(sentinel, first);
+      updateEdge();
+      size++;
+    }
+  }
+  private void updateEdge(){
+    updateLast();
+    updateFirst();
+  }
+
+  /**
+   *  Adds an item of type T to the back of the deque.
+   * @param item
+   */
+  public void addLast(T item){
+    if (isEmpty())
+      addToEmpty(item);
+    else {
+      Node p = new Node(item);
+      p.bridgeNodes(last, sentinel);
+      updateEdge();
+      size++;
+    }
+  }
+
+  /**
+   * @return true if deque is empty, false otherwise.
+   */
+  public boolean isEmpty(){
+    return size == 0;
+  }
+
+  /**
+   * @return the number of items in the deque.
+   */
+  public int size(){
+    return this.size;
+  }
+
+  /**
+   * Prints the items in the deque from first to last, separated by a space.
+   * Once all the items have been printed, print out a new line.
+   */
+  public void printDeque(){
+    Node p = first;
+    for (int i=0; i < size; i++){
+      System.out.println(p.item);
+      p = p.next;
+    }
+  }
+
+  /**
+   * Removes and returns the item at the front of the deque.
+   * If no such item exists, returns null.
+   * @return the item at the front of the deque
+   */
+  public T removeFirst(){
+    if (isEmpty())
+      return null;
+    if (size == 1)
+      return removeOnlyItem();
+    T itemRemoved = first.item;
+    sentinel.doubleLink(first.next);
+    updateEdge();
+    size--;
+    return itemRemoved;
+  }
+
+  /**
+   * Handle case that there is only item in the list, and you want to remove it.
+   * In this case, both fist and last should be updated.
+   *  Return the removed item. If there is more than 1 item, return null.
+   * @return
+   */
+  private T removeOnlyItem(){
+    if (size != 1)
+      return null;
+    T itemRemoved = first.item;
+    sentinel.bridgeNodes(null, null);
+    updateFirst();
+    updateLast();
+    size = 0;
+    return itemRemoved;
+  }
+  /**
+   * Removes and returns the item at the back of the deque.
+   * If no such item exists, returns null.
+   * @return the item at the front of the deque
+   */
+  public T removeLast(){
+    if (size <= 1)
+      return removeFirst();
+    T itemRemoved = last.item;
+    last.prev.doubleLink(sentinel);
+    updateEdge();
+    size--;
+    return itemRemoved;
+  }
+
+  /**
+   * Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+   * If no such item exists, returns null.
+   * @param index
+   * @return
+   */
+  public T get(int index){
+    if (index >= size){
+      return null;
+    }
+    Node p = first;
+    while (index != 0){
+      p = p.next;
+      index--;
+    }
+    return p.item;
+  }
+
+
+
+
+}
--- proj1/deque/LinkedListDequeTest.java
+++ proj1/deque/LinkedListDequeTest.java
@@ -1,6 +1,7 @@
 package deque;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 
@@ -8,14 +9,32 @@ import static org.junit.Assert.*;
 public class LinkedListDequeTest {
 
     @Test
+    public void getTest(){
+        LinkedListDeque<String> lls = new LinkedListDeque<>("Anchor");
+        for (int i=0; i < 100; i++){
+            lls.addFirst("hello");
+        }
+        for (int i=0; i < 99; i++){
+            lls.addLast("world");
+        }
+        String expect = "Anchor";
+        String actual = lls.get(100);
+        assertEquals(expect, actual);
+    }
+    @Test
+    public void initLListTest(){
+        LinkedListDeque<String> lls = new LinkedListDeque<>("hi");
+        int expect = 1;
+        int actual = lls.size();
+        assertEquals(expect,actual);
+    }
+    @Test
     /** Adds a few things to the list, checking isEmpty() and size() are correct,
      * finally printing the results.
      *
      * && is the "and" operation. */
     public void addIsEmptySizeTest() {
 
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
         LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 
 		assertTrue("A newly initialized LLDeque should be empty", lld1.isEmpty());
@@ -34,15 +53,13 @@ public class LinkedListDequeTest {
 
 		System.out.println("Printing out deque: ");
 		lld1.printDeque();
-		*/
     }
+
 
     @Test
     /** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
     public void addRemoveTest() {
 
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
 		// should be empty
 		assertTrue("lld1 should be empty upon initialization", lld1.isEmpty());
@@ -54,15 +71,12 @@ public class LinkedListDequeTest {
 		lld1.removeFirst();
 		// should be empty
 		assertTrue("lld1 should be empty after removal", lld1.isEmpty());
-        */
     }
 
     @Test
     /* Tests removing from an empty deque */
     public void removeEmptyTest() {
 
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<>();
         lld1.addFirst(3);
 
@@ -77,14 +91,12 @@ public class LinkedListDequeTest {
         errorMsg += "  actual size() returned 0\n";
 
         assertEquals(errorMsg, 0, size);
-        */
     }
 
     @Test
     /* Check if you can create LinkedListDeques with different parameterized types*/
     public void multipleParamTest() {
 
-        /*
         LinkedListDeque<String>  lld1 = new LinkedListDeque<String>();
         LinkedListDeque<Double>  lld2 = new LinkedListDeque<Double>();
         LinkedListDeque<Boolean> lld3 = new LinkedListDeque<Boolean>();
@@ -96,15 +108,12 @@ public class LinkedListDequeTest {
         String s = lld1.removeFirst();
         double d = lld2.removeFirst();
         boolean b = lld3.removeFirst();
-        */
     }
 
     @Test
     /* check if null is return when removing from an empty LinkedListDeque. */
     public void emptyNullReturnTest() {
 
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
 
         boolean passed1 = false;
@@ -112,28 +121,24 @@ public class LinkedListDequeTest {
         assertEquals("Should return null when removeFirst is called on an empty Deque,", null, lld1.removeFirst());
         assertEquals("Should return null when removeLast is called on an empty Deque,", null, lld1.removeLast());
 
-        */
     }
 
     @Test
     /* Add large number of elements to deque; check if order is correct. */
     public void bigLLDequeTest() {
 
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-        /*
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < 6; i++) {
             lld1.addLast(i);
         }
 
-        for (double i = 0; i < 500000; i++) {
+        for (double i = 0; i < 3; i++) {
             assertEquals("Should have the same value", i, (double) lld1.removeFirst(), 0.0);
         }
 
-        for (double i = 999999; i > 500000; i--) {
+        for (double i = 5; i > 3; i--) {
             assertEquals("Should have the same value", i, (double) lld1.removeLast(), 0.0);
         }
 
-        */
     }
 }
