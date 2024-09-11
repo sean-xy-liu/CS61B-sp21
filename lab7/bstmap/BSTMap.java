3f9c0d7a119d1d872a7aa95a1d14e5899312eb1d
Sean Liu <liuxinyong083@gmail.com>
2024-09-11 12:28:57 +0800

    Finished most methods of BSTMap, but there is still some bugs in get and put, going to implement these two methods from scratch.

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,128 @@
+package bstmap;
+
+import java.util.Iterator;
+import java.util.Set;
+
+// K have compareTo method.
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
+    BSTNode root;
+    int size;
+    private class BSTNode {
+       K key;
+       V value;
+       BSTNode left, right;
+       void setLeft(BSTNode leftNode) {
+           left = leftNode;
+       }
+       void setRight(BSTNode rightNode) {
+           right = rightNode;
+       }
+       int compareTo(BSTNode other) {
+           return key.compareTo(other.key);
+       }
+       BSTNode(K key, V value) {
+           this.key = key;
+           this.value = value;
+       }
+    }
+    public void printInOrder() {
+        printInOrder(root);
+    }
+    private void printInOrder(BSTNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println("key: " + node.key + " value: " + node.value);
+        printInOrder(node.left);
+        printInOrder(node.right);
+    }
+    @Override
+    public void clear() {
+        root = null;
+        size = 0;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return get(key, root) != null;
+    }
+
+    @Override
+    public V get(K key) {
+        BSTNode p = get(key, root);
+        if (p == null) {
+            return null;
+        }
+        return p.value;
+    }
+    private BSTNode get(K key, BSTNode node) {
+        if (node == null) {
+            return null;
+        }
+        if (node.key == key) {
+            return node;
+        } else if (key.compareTo(node.key) > 0) {
+            return get(key, node.left);
+        } else {
+            return get(key, node.right);
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        if (root == null) {
+            root = new BSTNode(key, value);
+            size++;
+        }
+        BSTNode p = get(key, root);
+        if (p != null) {
+            p.value = value;
+        } else {
+            put(key, value, root);
+        }
+    }
+    // node shouldn't be null and the map doesn't contain this key
+    private void put(K key, V value, BSTNode node) {
+        int compareResult = key.compareTo(node.key);
+        if (compareResult > 0) {
+            if (node.right == null) {
+                node.setRight(new BSTNode(key, value));
+                size++;
+            } else {
+                put(key, value, node.right);
+            }
+        } else {
+            if (node.left == null) {
+                node.setLeft(new BSTNode(key, value));
+                size++;
+            } else {
+                put(key, value, node.left);
+            }
+        }
+    }
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+}
